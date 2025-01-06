9e49fb85a6006e1cd4fa9fa0de170ab0ecd570c5
RUDRA PRASANNA MISHRA <rudrapmishra_en24b18_117@dtu.ac.in>
2025-01-06 17:20:57 +0000

    commit

--- 01_Basics/01_Basics.c
+++ 01_Basics/01_Basics.c
@@ -4,3 +4,9 @@ int main() {
     printf("This is my first C Program");
     return 0;
 }
+
+// #include <stdio.h> is a header file library that lets uswork with input and output functions.
+// main() This is called a function. Any code inside its curlybrackets {} will be executed.
+// the printf() is a function used to output/print text to the screen.
+//****************** Every C statement ends with a semicolon *************************
+// return 0 ends the main() function
