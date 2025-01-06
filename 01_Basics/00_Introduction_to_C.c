7a925786f24c1f00ca0923f8a554389f75fb990a
RUDRA PRASANNA MISHRA <rudrapmishra_en24b18_117@dtu.ac.in>
2025-01-06 17:14:52 +0000

    commit

---
+++ 01_Basics/00_Introduction_to_C.c
@@ -0,0 +1,32 @@
+/** Computer languages: 
+
+Computer languages are formal systemsdesigned to communicate instructions to acomputer.
+Essential for software development, computerlanguages serve as the foundation 
+for creating applications, websites, and various softwaresolutions.
+
+Types of Computer Languages:
+1) High-level languages: These languages are more abstract, making them 
+   easier for humans to understand and write. 
+   Use- from web development to artificial intelligence.
+   Ex- Python, Java.
+
+2) Low-level languages: Closer to machine language, provide more direct control over hardware.
+Use- crucial for tasks that require precise control over hardware resources.
+Ex- Assembly , C and Machine Code.
+
+3) Scripting languages: Interpreted languages used for automating tasks 
+and enhancing web development.
+Use- integral to web development, automation, and the creation of dynamic content.
+Ex- JavaScript and PHP
+
+Compiler: A Compiler serves as a translator, converting High-Level 
+Languageinput into low-level language. It transforms programming 
+language codes into machine code.
+
+Interpretor: The interpreter converts high-level language to an intermediate language. 
+An Interpreter works line by line on a code.
+
+WHAT IS C?
+C is a programming language.
+C is one of the oldest and finest programming languages.
+*/
